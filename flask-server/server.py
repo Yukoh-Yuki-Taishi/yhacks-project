c9c2b57e26de3eb3200946ad8d49bb4ccc7702c6
Taishi Nishizawa <taishi.nishizawa201@gmail.com>
2022-04-09 14:37:18 -0400

    feature: trying to connect flask w frontend

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
- 
+from summarization import summarize_txt
+# from suggestion import 
+
+
 app = Flask(__name__)
 CORS(app)
  
@@ -8,10 +11,13 @@ CORS(app)
 def test():
    return jsonify({"Result": request.json['transcript']})
  
-# @app.route('/summarize', methods =['POST'])
-# def summarize():
-# # call the summary function here to get the summary
-#    return jsonify({"Result": request.json['transcript']})
+@app.route('/summarize', methods =['POST'])
+def summarize():
+# call the summary function here to get the summary
+   txt = "yo"
+   summary = summarize(txt) # of type text
+   return jsonify({"Result": request.json['transcript']})
+ 
 
 if __name__ == '__main__':
     app.run(debug = True)
---
+++ flask-server/summarization.py
@@ -0,0 +1,21 @@
+import re
+from gensim.summarization import summarize
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+
+def summarize_txt(txt, ratio=False, nwords=False):
+    xt = re.sub(r'\n|\r', ' ', txt)
+    txt = re.sub(r' +', ' ', txt)
+    txt = txt.strip()
+
+    if nwords:
+        return summarize(txt, word_count=nwords, split=False)
+    if not ratio:
+        ratio = 0.2
+    return summarize(txt, ratio=ratio, split=False)
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        txt = ' '.join(f.readlines())
