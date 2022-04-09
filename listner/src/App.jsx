8a67ae77ffe520195e78042f91397d3267a1232e
Taishi Nishizawa <taishi.nishizawa201@gmail.com>
2022-04-09 00:58:13 -0400

    FEATURE: routing and component structure

---
+++ listner/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import Voice from './Components/Voice/Voice';
+// import Record from './Components/Record';
+import StartPage from './Components/StartPage/StartPage';
+import Suggestions from './Components/Suggestions/Suggestions';
+import Final from './Components/Final/Final';
+
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={ <StartPage/> } /> 
+        <Route path="/voice" element={ <Voice/>  } />
+        <Route path="/suggestions" element={ <Suggestions/>  } />
+        <Route path="/final" element={ <Final/>  } />
+        {/* <Route path="/record" element={ <Record/>  } /> */}
+        {/* <Record/> */}
+      </Routes>
+      {/* <Voice/>  */}
+    </div>
+  );
+}
+
+export default App;
---
+++ listner/src/Components/Suggestions/Suggestions.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+function Suggestions() {
+  return (
+    <div>
+        <h1>SUGGESTIONS</h1>
+        <Link to="/voice"> Go back</Link>
+    </div>
+  )
+}
+
+export default Suggestions
