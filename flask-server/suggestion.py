c1507cd0f7d40eb3e0c5cad9e0e6ee3246a31f18
Taishi Nishizawa <taishi.nishizawa201@gmail.com>
2022-04-09 19:55:10 -0400

    FEATURE: updated suggestion

--- flask-server/suggestion.py
+++ flask-server/suggestion.py
@@ -1,3 +1,8 @@
+from punctuator import Punctuator
+import warnings
+import os
+from gensim.summarization import keywords
+from gensim.models import KeyedVectors
 import spacy
 import yake
 from collections import Counter
@@ -6,11 +11,6 @@ import pytextrank
 from nltk.corpus import wordnet
 import nltk
 nltk.download("wordnet")
-from gensim.models import KeyedVectors
-from gensim.summarization import keywords
-# from sklearn.metrics.pairwise import cosine_similarity
-import os
-import warnings
 
 os.environ["SPACY_WARNING_IGNORE"] = "W008"
 warnings.filterwarnings("ignore", message=r"\[W008\]", category=UserWarning)
@@ -21,10 +21,13 @@ class TextAnalyzer():
     def __init__(self, model="en_core_web_lg", txt=None, level=1):
         self.nlp = spacy.load(model)
         if not txt:
-            with open("input.txt") as f:
-                self.txt = ' '.join(f.readlines())
-        else:
-            self.txt = txt
+            try:
+                with open("input.txt") as f:
+                    txt = ' '.join(f.readlines())
+            except Exception as e:
+                print("You must provide text or input.txt")
+                exit()
+        self.txt = self.add_punctuation(txt)
         self._pos_tag = ['PROPN', 'ADJ', 'NOUN']
         self.orig_doc = self.nlp(self.txt)
         self.language = "en"
@@ -34,6 +37,10 @@ class TextAnalyzer():
             self.thresh = 0.01
         else:
             self.thresh = 0.005
+
+    def add_punctuation(self, txt):
+        p = Punctuator('models/Demo-Europarl-EN.pcl')
+        return p.punctuate(txt)
 
     def get_keywords_spacy(self, n):
         result = []
@@ -49,7 +56,7 @@ class TextAnalyzer():
 
     def get_keywords_yake(self, n, max_ngram=3):
         result = []
-        
+
         kw_extractor = yake.KeywordExtractor(
             lan=self.language, n=max_ngram, dedupLim=0.9, top=n, features=None)
         return kw_extractor.extract_keywords(self.txt)
@@ -85,7 +92,7 @@ class TextAnalyzer():
 
             if p > self.thresh:
                 phrase_similarity.append([phrase, p])
-        
+
         phrase_similarity.sort(key=lambda x: x[1], reverse=True)
 
         if nwords and nwords < len(phrase_similarity):
@@ -105,10 +112,9 @@ class TextAnalyzer():
 
 if __name__ == "__main__":
     txt = """
-    Your only chance of survival, if you are sincerely smitten, lies in hiding this fact from the woman you love, of feigning a casual detachment under all circumstances. What sadness there is in this simple observation! What an accusation against man! However, it had never occurred to me to contest this law, nor to imagine disobeying it: love makes you weak, and the weaker of the two is oppressed, tortured and finally killed by the other, who in his or her turn oppresses, tortures and kills without having evil intentions, without even getting pleasure from it, with complete indifference; that’s what men, normally, call love.
-    Christ, he thinks, by my age I ought to know. You don’t get on by being original. You don’t get on by being bright. You don’t get on by being strong. You get on by being a subtle crook; somehow he thinks that’s what Norris is, and he feels an irrational dislike taking root, and he tries to dismiss it, because he prefers his dislikes rational, but after all, these circumstances are extreme, the cardinal in the mud, the humiliating tussle to get him back in the saddle, the talking, talking on the barge, and worse, the talking, talking on his knees, as if Wolsey’s unravelling, in a great unweaving of scarlet thread that might lead you back into a scarlet labyrinth, with a dying monster at its heart.
+    your only chance of survival if you are sincerely smitten lies in hiding this fact from the woman you love of feigning a casual detachment under all circumstances what sadness there is in this simple observation what an accusation against man however it had never occurred to me to contest this law nor to imagine disobeying it love makes you weak and the weaker of the two is oppressed tortured and finally killed by the other who in his or her turn oppresses tortures and kills without having evil intentions without even getting pleasure from it with complete indifference that’s what men normally call love christ he thinks by my age I ought to know You don’t get on by being original you don’t get on by being bright You don’t get on by being strong You get on by being a subtle crook somehow he thinks that’s what Norris is and he feels an irrational dislike taking root and he tries to dismiss it because he prefers his dislikes rational but after all these circumstances are extreme the cardinal in the mud the humiliating tussle to get him back in the saddle the talking talking on the barge and worse the talking talking on his knees as if Wolsey’s unravelling in a great unweaving of scarlet thread that might lead you back into a scarlet labyrinth with a dying monster at its heart
     """
+    
     t = TextAnalyzer(txt=txt)
     output = t.run()
     print(output)
-
