03df571d0ac9880c36b06c6a7bfe4aa9b5d53ff1
yuki-hayashita <yuki_hayashita@brown.edu>
2022-04-09 17:50:04 -0400

    send as email

--- listener/src/Components/Final/Final.jsx
+++ listener/src/Components/Final/Final.jsx
@@ -1,17 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from "react-router-dom";
-import PrettyTextBox from "../Textbox/PrettyTextBox"
+import PrettyTextBox from "../Textbox/PrettyTextBox";
+import{ init } from '@emailjs/browser';
+import emailjs from '@emailjs/browser';
+init("F2qcJ1axt7vjzG28m");
+
 
 function Final(props) {
+  const [email, setEmail] = useState('');
+  const [sentence, setSentence] = useState('Send as Email');
+
   // const {transcript} = this.props.location;
   // console.log(props.location.state);
   const location = useLocation();
 
+  const templateParams = {
+    email: email,
+    transcript: location.state,
+  };
+
+  const sendEmail = () => {
+    if (email !== ''){
+      emailjs.send('service_jkgii6i', 'template_gnmcs29', templateParams)
+      .then(function(response) {
+          console.log('SUCCESS!', response.status, response.text);
+          setSentence('Sent　✅')
+        }, function(error) {
+          console.log('FAILED...', error);
+        });
+    } else {
+      alert("type email address");
+    }
+  }
+
   return (
     <div>
-        <h1>FINAL PAGE</h1>
-        <Link to="/" >Click to restart Recording</Link>
+        <h1>Final Transcript</h1>
         <PrettyTextBox text = {location.state}/>
+        <input type="email" name="user_email" onChange={(e)=>setEmail(e.target.value)}/>
+        <button type="button" onClick={sendEmail}>{sentence}</button>
+        <br/>
+        <Link to="/" >Click to restart Listening</Link>
     </div>
   )
 }
